0554505ff7d098ef10690263c58210a13269d0a1
Salmaan <salmaan.arif.ali@gmail.com>
2021-01-25 18:51:18 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,41 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = doc.css(".student-card a")
+
+    students.map do |student|
+      student = {
+        :name => student.css("div.card-text-container .student-name").text,
+        :location => student.css("div.card-text-container .student-location").text,
+        :profile_url => student.attribute("href").value
+      }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+
+    student_info = {
+      :profile_quote => doc.css("div.profile-quote").text,
+      :bio => doc.css("p").text
+    }
+
+    doc.css("div.social-icon-container a").each do |link|
+      site = link.attribute("href").value
+      key = :blog
+      key = :twitter if site.include?("twitter")
+      key = :linkedin if site.include?("linkedin")
+      key = :github if site.include?("github")
+
+      student_info[key] = link.attribute("href").value
+    end
+
+    student_info
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,30 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      self.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      @twitter = value if key.to_s == "twitter"
+      @linkedin = value if key.to_s == "linkedin"
+      @github = value if key.to_s == "github"
+      @blog = value if key.to_s == "blog"
+      @profile_quote = value if key.to_s == "profile_quote"
+      @bio = value if key.to_s == "bio"
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
